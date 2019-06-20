d34d32801d907ee8144ab92e490c9450006c3ab6
Tatiane Hirata <tatihirata@outlook.com>
2019-06-20 19:59:28 -0400

    Inserção dos markers e criação dos dados dos locais do mapa

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import './App.css';
+import dataPlaces from './dataPlaces';
 //import Map from './Map.js';
 
 class App extends React.Component {
+  
+  state = {
+    dataPlaces : dataPlaces,
+    markers: [],
+
+  }
+
 
   componentDidMount() {
     this.showMap()
@@ -15,10 +23,48 @@ class App extends React.Component {
 
   initMap = () => {
       let map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: -15.5979387 , lng: -56.0930488},
+      center: {lat: -15.5926919, lng: -56.0903047},
       zoom: 13.75,
       mapTypeControl: false
-    })
+    });
+
+    let largeInfowindow = new window.google.maps.InfoWindow();
+    let bounds = new window.google.maps.LatLngBounds();
+
+    for(let i = 0; i < dataPlaces.length; i++) {
+      let position = dataPlaces[i].location;
+      let title = dataPlaces[i].title;
+      let placecontent = dataPlaces[i].content;
+
+      let marker = new window.google.maps.Marker({
+        map: map,
+        position: position,
+        title: title,
+        animation: window.google.maps.Animation.DROP,
+        id: i,
+        content: placecontent
+      })
+
+      this.state.markers.push(marker);
+      bounds.extend(marker.position);
+      marker.addListener('click', function() {
+        populateInfoWindow(this, largeInfowindow);
+      })
+    }    
+
+    map.fitBounds(bounds);
+
+    function populateInfoWindow(marker, infowindow) {
+      if(infowindow.marker != marker) {
+        infowindow.marker = marker;
+        infowindow.setContent('<div>' + marker.content + '</div>');
+        infowindow.open(map, marker);
+        infowindow.addListener('closeclick', function() {
+          infowindow.setMap(null)
+        })
+      }
+    }
+
   }  
 
 
@@ -27,10 +73,11 @@ class App extends React.Component {
       <div className="App">
   
         {/* MAP CONTAINER */}
-          <div id="map"></div>
+          <div id="map" role="aplication" tabIndex='-1'></div>
+          
+          
           {/* LIST OF SIGHTS */}
-
-          <aside>
+          <aside className="filter">
             <input type='text'></input>
                   <ul className="places-options">
                     <li><a href="#">Sesc Arsenal</a></li>
---
+++ src/dataPlaces.js
@@ -0,0 +1,53 @@
+const dataPlaces = [
+    {title:'Sesc Arsenal', 
+        location: {lat:-15.608194, lng:-56.103927},
+        content: 'Espaço de cultura dentro de Cuiabá'
+    },
+    
+    {title:'Parque das Águas', 
+        location: {lat:-15.568753, lng:-56.080079},
+        content: 'Parque com grande espaço para a natureza'
+    },
+    
+    {title:'Museu do Rio', 
+        location: {lat:-15.614890, lng: -56.107650},
+        content: 'Antigo mercado de peixes de Cuiabá'
+    },
+    
+    {title:'Arena Pantanal', 
+        location: {lat:-15.603957, lng:-56.121625},
+        content: 'Arena multiúso da cidade'
+    },
+    
+    {title:'Parque Tia Nair', 
+        location: {lat:-15.594809, lng:-56.059334},
+        content: 'Faça caminhada em um local agradável'
+    },
+    
+    {title:'Feira do Porto', 
+        location: {lat:-15.613978, lng: -56.110226},
+        content: 'Feira tradicional da cidade'
+    },
+    
+    {title:'Praça Popular', 
+        location: {lat:-15.593383, lng:-56.105925},
+        content: 'Vida noturna com vários bares ao redor'
+    },
+    
+    {title:'Praça da Mandioca', 
+        location: {lat:-15.592769, lng: -56.093851},
+        content: 'Bares na parte central de Cuiabá'
+    },
+    
+    {title:'Igreja Bom Despacho', 
+        location: {lat:-15.601677, lng:-56.096093},
+        content: 'Antigo seminário'
+    },
+    
+    {title:'Igreja do Rosário', 
+        location: {lat:-15.595278, lng:-56.091953},
+        content: 'Local da religiosidade dos cuiabanos'
+    }
+]
+
+export default dataPlaces
