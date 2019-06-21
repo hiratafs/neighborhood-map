fef53949eb0dd7f2095acc6d0af97d29d0014071
hiratafs <tatihirata@outlook.com>
2019-06-21 15:45:49 -0400

    Adicionados mais 7 locais em Cuiabá

--- src/dataPlaces.js
+++ src/dataPlaces.js
@@ -47,7 +47,44 @@ const dataPlaces = [
     {title:'Igreja do Rosário', 
         location: {lat:-15.595278, lng:-56.091953},
         content: 'Local da religiosidade dos cuiabanos'
+    },
+
+    {title: 'Raposa Vegana',
+        location: {lat:-15.591022, lng:-56.103373},
+        content: 'comida vegana'
+    },
+
+    {title: 'Arado Natural',
+        location: {lat:-15.593775, lng:-56.109329},
+        content: 'Comida vegetariana'
+    },
+
+    {title: 'Burguer na Praça',
+        location: {lat:-15.596384, lng: -56.107118},
+        content: 'Hamburguer para todos os gostos'
+    },
+
+    {title: 'Shopping Estação Cuiabá',
+        location: {lat:-15.590811, lng:-56.119548},
+        content: 'Mais novo shopping da cidade'
+    },
+
+    {title: 'Pantanal Shopping',
+        location: {lat:-15.575475, lng:-56.072445},
+        content: 'Shopping na Av. Historiador Rubens de Mendonça'
+    },
+
+    {title: 'Goiabeiras Shopping',
+        location: {lat:-15.587724, lng: -56.111829},
+        content: 'O primeiro shopping cuiabano'
+    },
+
+    {title: 'Shopping 3 Américas',
+        location: {lat:-15.612694, lng:-56.074783},
+        content: 'Shopping próximo da UFMT'
     }
+
+    
 ]
 
 export default dataPlaces
