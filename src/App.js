78b0e60c433704f464961f8067cac287ebdbf633
hiratafs <tatihirata@outlook.com>
2019-06-24 15:30:24 -0400

    inserindo href no a para evitar mensagens de erro

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import './App.css';
-import dataPlaces from './dataPlaces';
-//import Map from './Map.js';
+import dataPlaces from './dataPlaces.js';
+import { GMAP_ID } from './dataAPI.js';
+//import { FS_ID } from './dataAPI.js';
+//import { FS_CLIENT_ID } from './dataAPI.js';
+import { getPlace } from './FoursquareAPI.js'
+ //import Map from './Map.js';
 
 class App extends React.Component {
   
@@ -10,18 +14,19 @@ class App extends React.Component {
     markers: [],
   }
 
+  
   //RENDER MAP
   componentDidMount() {
     this.showMap()
   }
 
   showMap = () => {
-    installMap('https://maps.googleapis.com/maps/api/js?key=AIzaSyB9FdQGy5y1yZLQcG20qvf2FwVeVu6UMsM&callback=initMap');
+    installMap(`https://maps.googleapis.com/maps/api/js?key${GMAP_ID}&callback=initMap`);
     window.initMap = this.initMap
   }
 
   //MAP CONFIGURATION 
-  initMap = () => {
+ initMap = () => {
       let map = new window.google.maps.Map(document.getElementById('map'), {
       center: {lat: -15.5926919, lng: -56.0903047},
       zoom: 13.75,
@@ -34,7 +39,7 @@ class App extends React.Component {
     for(let i = 0; i < dataPlaces.length; i++) {
       let position = dataPlaces[i].location;
       let title = dataPlaces[i].title;
-      let placecontent = dataPlaces[i].content;
+      
 
       let marker = new window.google.maps.Marker({
         map: map,
@@ -42,7 +47,7 @@ class App extends React.Component {
         title: title,
         animation: window.google.maps.Animation.DROP,
         id: i,
-        content: placecontent
+
       })
 
       this.state.markers.push(marker);
@@ -55,9 +60,9 @@ class App extends React.Component {
     map.fitBounds(bounds);
 
     function populateInfoWindow(marker, infowindow) {
-      if(infowindow.marker != marker) {
+      if(infowindow.marker !== marker) {
         infowindow.marker = marker;
-        infowindow.setContent('<div>' + marker.content + '</div>');
+        infowindow.setContent('<div>' + marker.title + '</div>');
         infowindow.open(map, marker);
         infowindow.addListener('closeclick', function() {
           infowindow.setMap(null)
@@ -65,15 +70,17 @@ class App extends React.Component {
       }
     }
 
-  }  
+  }   
 
 
   render () {
+    dataPlaces.map((place) => {
+      return getPlace(place)
+    });
     return (
       <div className="App">
-  
-        {/* MAP CONTAINER */}
-          <div id="map" role="aplication" tabIndex='-1'></div>
+          {/* MAP CONTAINER */}
+          <div id="map" tabIndex='-1'></div>
           
           
           {/* LIST OF SIGHTS */}
@@ -84,23 +91,23 @@ class App extends React.Component {
             </div>
                 <div className="places-options">
                   <ul>
-                      <li><a href="#">Sesc Arsenal</a></li>
-                      <li><a href="#">Parque das Águas</a></li>
-                      <li><a href="#">Orla do Porto</a></li>
-                      <li><a href="#">Arena Pantanal</a></li>
-                      <li><a href="#">Parque Tia Nair</a></li>
-                      <li><a href="#">Feira do Porto</a></li>
-                      <li><a href="#">Praça Popular</a></li>
-                      <li><a href="#">Praça da Mandioca</a></li>
-                      <li><a href="#">Igreja Bom Despacho</a></li>
-                      <li><a href="#">Igreja do Rosário</a></li>
-                      <li><a href="#">Raposa Vegana</a></li>
-                      <li><a href="#">Arado Natural</a></li>
-                      <li><a href="#">Burguer na Praça</a></li>
-                      <li><a href="#">Shopping Estação Cuiabá</a></li>
-                      <li><a href="#">Goiabeiras Shopping</a></li>
-                      <li><a href="#">Pantanal Shopping</a></li>
-                      <li><a href="#">Shopping 3 Américas</a></li>
+                      <li><a href="http://localhost:3000">Sesc Arsenal</a></li>
+                      <li><a href="http://localhost:3000">Parque das Águas</a></li>
+                      <li><a href="http://localhost:3000">Orla do Porto</a></li>
+                      <li><a href="http://localhost:3000">Arena Pantanal</a></li>
+                      <li><a href="http://localhost:3000">Parque Tia Nair</a></li>
+                      <li><a href="http://localhost:3000">Feira do Porto</a></li>
+                      <li><a href="http://localhost:3000">Praça Popular</a></li>
+                      <li><a href="http://localhost:3000">Praça da Mandioca</a></li>
+                      <li><a href="http://localhost:3000">Igreja Bom Despacho</a></li>
+                      <li><a href="http://localhost:3000">Igreja do Rosário</a></li>
+                      <li><a href="http://localhost:3000">Raposa Vegana</a></li>
+                      <li><a href="http://localhost:3000">Arado Natural</a></li>
+                      <li><a href="http://localhost:3000">Burguer na Praça</a></li>
+                      <li><a href="http://localhost:3000">Shopping Estação Cuiabá</a></li>
+                      <li><a href="http://localhost:3000">Goiabeiras Shopping</a></li>
+                      <li><a href="http://localhost:3000">Pantanal Shopping</a></li>
+                      <li><a href="http://localhost:3000">Shopping 3 Américas</a></li>
                   </ul>
                 </div>
           </aside>
