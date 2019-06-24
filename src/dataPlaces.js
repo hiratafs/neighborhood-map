e6631d7f3f665befec9c6925d9dd66688052ba37
hiratafs <tatihirata@outlook.com>
2019-06-24 15:31:24 -0400

    adicionado venueID e address

--- src/dataPlaces.js
+++ src/dataPlaces.js
@@ -1,89 +1,139 @@
 const dataPlaces = [
     {title:'Sesc Arsenal', 
-        location: {lat:-15.608194, lng:-56.103927},
-        content: 'Espaço de cultura dentro de Cuiabá'
+        location: {
+            address: 'R. Treze de Junho (R. Cel. Benedito Leite), Porto',
+            lat:-15.608194, 
+            lng:-56.103927},
+        venueID: '4bec7921b68520a18c2b1287'
     },
     
     {title:'Parque das Águas', 
-        location: {lat:-15.568753, lng:-56.080079},
-        content: 'Parque com grande espaço para a natureza'
+        location: {
+            address: 'Av. Hermina Torquarto da Silva, Centro Político Administrativo',
+            lat:-15.568753, 
+            lng:-56.080079},
+        venueID: '5866ee8e424f937f62aa4350'
     },
     
     {title:'Museu do Rio', 
-        location: {lat:-15.614890, lng: -56.107650},
-        content: 'Antigo mercado de peixes de Cuiabá'
+        location: {
+            address: 'Av. Manoel José de Arruda, 1899, Porto',
+            lat:-15.614890, 
+            lng: -56.107650},
+        venueID: '4ebc2614cc2178857ec7a254'
     },
     
     {title:'Arena Pantanal', 
-        location: {lat:-15.603957, lng:-56.121625},
-        content: 'Arena multiúso da cidade'
+        location: {
+            address: 'Av. Agrícola Paes de Barros, s/n, Verdão',
+            lat:-15.603957, 
+            lng:-56.121625},
+        venueID: '4e09f18a6365ecbd5d5674d2'
     },
     
     {title:'Parque Tia Nair', 
-        location: {lat:-15.594809, lng:-56.059334},
-        content: 'Faça caminhada em um local agradável'
+        location: {
+            address: 'Av. Italia, s/n, Jardim Itália',
+            lat:-15.594809, 
+            lng:-56.059334},
+        venueID: '566b5011498eb66bfda38b89'
     },
     
-    {title:'Feira do Porto', 
-        location: {lat:-15.613978, lng: -56.110226},
-        content: 'Feira tradicional da cidade'
+    {title:'Mercado do Porto', 
+        location: {
+            address: 'Av. Oito de Abril, Porto',
+            lat:-15.613978, 
+            lng: -56.110226},
+        venueID: '4c4b021cbad7a59395fe3dab'
     },
     
     {title:'Praça Popular', 
-        location: {lat:-15.593383, lng:-56.105925},
-        content: 'Vida noturna com vários bares ao redor'
+        location: {
+            address: 'R. Brig. Eduardo Gomes, Goiabeiras',
+            lat:-15.593383, 
+            lng:-56.105925},
+        venueID: '4c9fc818d3c2b60cd9b3d7bc'
     },
     
     {title:'Praça da Mandioca', 
-        location: {lat:-15.592769, lng: -56.093851},
-        content: 'Bares na parte central de Cuiabá'
+        location: {
+            address: 'Rua Pedro Celestino, s/n, Centro',
+            lat:-15.592769, 
+            lng: -56.093851},
+        venueID: '4cfd1d757f2db1f7d35836d4'
     },
     
-    {title:'Igreja Bom Despacho', 
-        location: {lat:-15.601677, lng:-56.096093},
-        content: 'Antigo seminário'
+    {title:'Igreja Nossa Senhora do Bom Despacho', 
+        location: {
+            address: 'Pç. do Seminário, Centro',
+            lat:-15.601677, 
+            lng:-56.096093},
+        venueID: '4e47da16d164155c0df7b122'
     },
     
-    {title:'Igreja do Rosário', 
-        location: {lat:-15.595278, lng:-56.091953},
-        content: 'Local da religiosidade dos cuiabanos'
+    {title:'Igreja do Rosário e São Benedito', 
+        location: {
+            address: 'Av. Cel. Escolástico (Praça do Rosário)',
+            lat:-15.595278, 
+            lng:-56.091953},
+        venueID: '4d449e06bf61a1cdd3d70aac'
     },
 
     {title: 'Raposa Vegana',
-        location: {lat:-15.591022, lng:-56.103373},
-        content: 'comida vegana'
+        location: {
+            address: 'Rua Cândido Mariano, 1270, Centro',
+            lat:-15.591022, 
+            lng:-56.103373},
+        venueID: '57e3fc0c498ec1b5d77d6629'
     },
 
     {title: 'Arado Natural',
-        location: {lat:-15.593775, lng:-56.109329},
-        content: 'Comida vegetariana'
+        location: {
+            address: 'Av. Sen. Filinto Müller, 284, Popular',
+            lat:-15.593775, 
+            lng:-56.109329},
+        venueID: '5a650f1265211f23f1b4074a'
     },
 
     {title: 'Burguer na Praça',
-        location: {lat:-15.596384, lng: -56.107118},
-        content: 'Hamburguer para todos os gostos'
+        location: {
+            address: 'Av. São Sebastião N. 2069, Popular',
+            lat:-15.596384, 
+            lng: -56.107118},
+        venueID: '59a38c2a5c68387308591b1d'
     },
 
     {title: 'Shopping Estação Cuiabá',
-        location: {lat:-15.590811, lng:-56.119548},
-        content: 'Mais novo shopping da cidade'
+        location: {
+            address: 'Av. Miguel Sutil, 9300, Santa Rosa ',
+            lat:-15.590811, 
+            lng:-56.119548},
+        venueID: '5bbf48f6911fc4002cd52994'
     },
 
     {title: 'Pantanal Shopping',
-        location: {lat:-15.575475, lng:-56.072445},
-        content: 'Shopping na Av. Historiador Rubens de Mendonça'
+        location: {
+            address: 'Av. Histo. Rubens de Mendonça, 3.300',
+            lat:-15.575475, 
+            lng:-56.072445},
+        venueID: '4b8abd3af964a5209d7d32e3'
     },
 
     {title: 'Goiabeiras Shopping',
-        location: {lat:-15.587724, lng: -56.111829},
-        content: 'O primeiro shopping cuiabano'
+        location: {
+            address: 'Av. José Monteiro de Figueiredo, 500, Duque de Caxias',
+            lat:-15.587724, 
+            lng: -56.111829},
+        venueID: '4b86a8a8f964a520b89431e3'
     },
 
     {title: 'Shopping 3 Américas',
-        location: {lat:-15.612694, lng:-56.074783},
-        content: 'Shopping próximo da UFMT'
+        location: {
+            address: 'Av. Brasília, 239, Jardim das Américas',
+            lat:-15.612694, 
+            lng:-56.074783},
+        venueID: '4b9d8e3ff964a52075b336e3'
     }
-
     
 ]
 
