f280d3bf843a4ee4c5e313f0729d8238dc9fa3b4
hiratafs <tatihirata@outlook.com>
2019-06-27 16:25:21 -0400

    Adicionado marcadores e infowindows estilizados

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import './App.css';
 import axios from 'axios';
+import Map from './Map'
+import Search from './Search'
+import imgNotAvailable from './img/business_large_square.png'
 
+//GOOGLE MAP ID
 const GMAP_ID = 'AIzaSyB9FdQGy5y1yZLQcG20qvf2FwVeVu6UMsM';
+
+//YELP API KEY
 const apikey = 'N9uoPrrbgvV9bZzZDgt0EfHLfVIpgtWXYpwno1jTgJbvbSfD1_1dN589w6Egvkp_QN-TKdUMtGohdENnIlRlXQu6sSd2b5_MhyUXmUT6af7mTLlneezTfR36InUSXXYx';
+
+//
 const herokucors = 'https://cors-anywhere.herokuapp.com/'
 const apiUrl = "https://api.yelp.com/v3/businesses/search";
 
-const configuration ={
+
+
+/* CONFIGURATION TO RETRIEVE YELP DATA */
+const configuration = {
   headers: {
     'Authorization': `Bearer ${apikey}`
   },
   params: { 
     location: 'Cuiaba',
-    categories: 'burger'
+    categories: 'restaurants'
   }
 }
 
@@ -28,21 +39,21 @@ class App extends React.Component {
   //RENDER MAP
   componentDidMount() {
     this.retrieveYelpData();
-    this.showMap()
   }
 
+  //FUNCTION TO RETRIEVE YELP DATA WITH AXIOS
   retrieveYelpData = () => {
     axios.get(`${herokucors}${apiUrl}`, configuration)
     .then(response => {
       if(response.status === 200) {
-        this.setState({cuiabaPlaces: response.data.businesses});
-        console.log(this.state.cuiabaPlaces[0].coordinates)
+        this.setState({cuiabaPlaces: response.data}, this.showMap());
+        console.log(this.state.cuiabaPlaces.businesses)
       }
     })
     .catch(err => console.log(err))
-    
   }
 
+  //FUNCTION TO SHOW MAP ON THE BROWSER
   showMap = () => {
     installMap(`https://maps.googleapis.com/maps/api/js?key${GMAP_ID}&callback=initMap`);
     window.initMap = this.initMap
@@ -56,28 +67,59 @@ class App extends React.Component {
       mapTypeControl: false
     });
 
-    //let largeInfowindow = new window.google.maps.InfoWindow();
+    //Array for Yelp markers
+    let YelpMarkers = [];
+
+    //Map array cuiabaPlaces from state then create a marker for each place
+    this.state.cuiabaPlaces.businesses.map(local => {
+      let marker = new window.google.maps.Marker({
+        position: {lat: local.coordinates.latitude, lng: local.coordinates.longitude},
+        map: map,
+        title: local.name,
+        animation: window.google.maps.Animation.DROP
+      })
+
+      //Infowindow design
+      let imgReview = local.image_url && local.image_url !== '' ? local.image_url : imgNotAvailable;
+
+      let contentString = `<div className="infowindow">` +
+      `<h1> ${local.name} </h1>` +
+      `<img style='max-width:200px' src=${imgReview} alt=${local.name} />` +
+      `<p><strong>Address:</strong> ${local.location.display_address} </p>` +
+      `<p><strong>Phone:</strong> ${local.phone} </p>` +
+      `<p><strong>Price:</strong> ${local.price} </p>` +
+      '</div>'
+
+   
+      //Insert markers into YelpMarkers array
+      YelpMarkers.push(marker);
+
+      //Add a event listener to each marker and open an infowindow
+      marker.addListener('click', function() {
+        populateInfoWindow(this, largeInfowindow, contentString)
+      })
+    })
+
+   
+
+    let largeInfowindow = new window.google.maps.InfoWindow({maxWidth:250});
     //let bounds = new window.google.maps.LatLngBounds();
 
-    for(let i = 0; i < this.state.cuiabaPlaces.length; i++) {
-      let position = {lat: this.state.cuiabaPlaces[i].coordinates.latitude,
-                      lng: this.state.cuiabaPlaces[i].coordinates.longitude }
-      let title = this.state.cuiabaPlaces[i].name;
-    
-    }
+          
        
     //map.fitBounds(bounds);
 
-    function populateInfoWindow(marker, infowindow) {
+   // FUNCTION TO DISPLAY INFOWINDOW
+    function populateInfoWindow(marker, infowindow, data) {
       if(infowindow.marker !== marker) {
         infowindow.marker = marker;
-        infowindow.setContent('<div>' + marker.title + '</div>');
+        infowindow.setContent(data);
         infowindow.open(map, marker);
         infowindow.addListener('closeclick', function() {
           infowindow.setMap(null)
         })
       }
-    }  
+    }   
 
   }   
 
@@ -86,33 +128,18 @@ class App extends React.Component {
     return (
       <div className="App">
           {/* MAP CONTAINER */}
-          <div id="map" tabIndex='-1'></div>
+            <Map />          
           
-          
-          {/* LIST OF SIGHTS */}
+          {/* LIST OF WHERE TO EAT IN CUIABA */}
           <aside className="filter">
-            <div className="search">
-              <label htmlFor="options"><span className="label-search">Search in Cuiabá</span></label>
-              <input type='text' id="options" placeholder="Type here"></input>
-            </div>
+              <Search />
                 <div className="places-options">
                   <ul>
-                      <li><a href="http://localhost:3000">Sesc Arsenal</a></li>
-                      <li><a href="http://localhost:3000">Parque das Águas</a></li>
-                      <li><a href="http://localhost:3000">Arena Pantanal</a></li>
-                      <li><a href="http://localhost:3000">Parque Tia Nair</a></li>
-                      <li><a href="http://localhost:3000">Feira do Porto</a></li>
-                      <li><a href="http://localhost:3000">Praça Popular</a></li>
-                      <li><a href="http://localhost:3000">Praça da Mandioca</a></li>
-                      <li><a href="http://localhost:3000">Igreja Bom Despacho</a></li>
-                      <li><a href="http://localhost:3000">Igreja do Rosário</a></li>
-                      <li><a href="http://localhost:3000">Raposa Vegana</a></li>
-                      <li><a href="http://localhost:3000">Arado Natural</a></li>
-                      <li><a href="http://localhost:3000">Burguer na Praça</a></li>
-                      <li><a href="http://localhost:3000">Shopping Estação Cuiabá</a></li>
-                      <li><a href="http://localhost:3000">Goiabeiras Shopping</a></li>
-                      <li><a href="http://localhost:3000">Pantanal Shopping</a></li>
-                      <li><a href="http://localhost:3000">Shopping 3 Américas</a></li>
+                      <li><a placename={this.state.cuiabaPlaces.name} href="http://localhost:3000">Sesc Arsenal</a></li>
+                      <li><a placename={this.state.cuiabaPlaces.name} href="http://localhost:3000">Parque das Águas</a></li>
+                      <li><a placename={this.state.cuiabaPlaces.name} href="http://localhost:3000">Arena Pantanal</a></li>
+                      <li><a placename={this.state.cuiabaPlaces.name} href="http://localhost:3000">Parque Tia Nair</a></li>
+                      <li><a placename={this.state.cuiabaPlaces.name} href="http://localhost:3000">Feira do Porto</a></li>
                   </ul>
                 </div>
           </aside>
