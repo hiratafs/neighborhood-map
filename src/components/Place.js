1d76d59a503517275de26f7b29cced9bbd56240b
hiratafs <tatihirata@outlook.com>
2019-07-05 14:53:17 -0400

    Modificado componente para listagem de cada restaurante

--- src/components/Place.js
+++ src/components/Place.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Place extends Component {
     render () {
         return(
-           <li><a href="www.google.com.br">Place</a></li>
+           <li>{this.props.name}</li>
         )
     }
 }
