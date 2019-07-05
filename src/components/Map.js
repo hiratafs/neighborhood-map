5935fbb28adc8cb5538d4bbb3bddcd213f375a58
hiratafs <tatihirata@outlook.com>
2019-07-05 14:52:42 -0400

    Inserido código do react-google-maps no componente

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+
+
+const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+  <GoogleMap
+    defaultZoom={13.75}
+    defaultCenter={{lat: -15.5926919, lng: -56.0903047}}
+  >
+    {props.markers.map((marker, id) => (
+        <Marker key={id} position={{ lat: marker.lat, lng: marker.lng}} />
+    )
+        )}
+  </GoogleMap>
+))
 
 class Map extends Component {
     render () {
         return(
-            <div id="map" tabIndex='-1'></div>
+            <MyMapComponent
+                {...this.props}
+                isMarkerShown
+                googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyB9FdQGy5y1yZLQcG20qvf2FwVeVu6UMsM"
+                loadingElement={<div style={{ height: `100%` }} />}
+                containerElement={<div style={{ height: `100%` }} />}
+                mapElement={<div style={{ height: `100%` }} />}
+            />
         )
     }
 }
