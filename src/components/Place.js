a04af7bfd462cdb7918fe24cf65dafb391418b1b
hiratafs <tatihirata@outlook.com>
2019-07-10 10:18:21 -0400

    Alterado array para listagem dos locais

--- src/components/Place.js
+++ src/components/Place.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 class Place extends Component {
+
+
     render () {
         return(
            <li onClick={() => this.props.handleClickList(this.props)} onKeyPress={() => this.props.handleClickList(this.props)} tabIndex="0">{this.props.name}</li>
