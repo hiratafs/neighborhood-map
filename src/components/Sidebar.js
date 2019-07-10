e0cc2f2d88fbfc2986875c8c6c62c58d625fa4a1
hiratafs <tatihirata@outlook.com>
2019-07-10 10:17:53 -0400

    Retirado state query do código

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,15 +4,14 @@ import List from './List'
 
 class Sidebar extends Component {
 
-    state = {
-        query: ''}
+            
 
     render () {
         return (
             <div >
                 <div className="search">
                 <label htmlFor="options"><span className="label-search">Restaurants in Cuiabá</span></label>
-                  <input type='search' id="search" placeholder="Filter restaurant" onChange={this.updateQuery}></input>
+                  <input type='search' id="search" placeholder="Filter restaurant" onChange={this.props.updateQuery}></input>
                 </div>
                   <List {...this.props} handleClickList={this.props.handleClickList}/>
             </div>
