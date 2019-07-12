0538171203cac4ca2a2f2b87fe2f5ee4e8d2e682
hiratafs <tatihirata@outlook.com>
2019-07-12 08:21:08 -0400

    Adicionado Error Boundaries

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import List from './List'
-
+import ErrorBoundaries from './ErrorBoundaries'
 
 class Sidebar extends Component {
            
@@ -11,7 +11,9 @@ class Sidebar extends Component {
                 <label htmlFor="options"><span className="label-search">Restaurants in Cuiabá</span></label>
                   <input type='search' id="search" placeholder="Filter restaurant" onChange={this.props.updateQuery}></input>
                 </div>
+                <ErrorBoundaries>
                   <List {...this.props} handleClickList={this.props.handleClickList}/>
+                  </ErrorBoundaries>
             </div>
          
         )
